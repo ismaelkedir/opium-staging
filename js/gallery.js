57a3ba3b2a29f11241d4c589292d2277a9c295f9
Ismael <kismaelkemal@gmail.com>
2017-10-22 17:51:46 +0300

    Added a image gallery page with masonary grid

---
+++ js/gallery.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.grid').masonry({
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: 195,
+        gutter: 3,
+        fitWidth: true,
+    });
+}());
